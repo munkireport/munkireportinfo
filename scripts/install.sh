7bc17c651c7339a4e461a9a2a9f2c3982806a6f7
tuxudo <tvxudo@gmail.com>
2019-03-29 11:47:46 -0400

    Cleaned up module and added support for profiles

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# munkireportinfo_controller
+NW_CTL="${BASEURL}index.php?/module/munkireportinfo/"
+
+# Get the script in the proper directory
+"${CURL[@]}" "${NW_CTL}get_script/munkireportinfo" -o "${MUNKIPATH}preflight.d/munkireportinfo"
+
+if [ "${?}" != 0 ]
+then
+	echo "Failed to download all required components!"
+	rm -f ${MUNKIPATH}preflight.d/munkireportinfo
+	exit 1
+fi
+
+# Make executable
+chmod a+x "${MUNKIPATH}preflight.d/munkireportinfo"
+
 # Set preference to include this file in the preflight check
-setreportpref "munkireportinfo" "/Library/Preferences/MunkiReport.plist"
+setreportpref "munkireportinfo" "${CACHEPATH}munkireportinfo.plist"
